db029cbc5bab0f75c1b0ec271117b392dd6a9f39
matt-winzer <matt.winzer@gmail.com>
2017-12-08 14:49:27 -0700

    landing page and projects page working

--- routes/projects.js
+++ routes/projects.js
@@ -4,7 +4,7 @@ const knex = require('../db/connection')
 const queries = require('../db/queries')
 
 router.get('/', (req, res) => {
-  queries.getAllProjects().then(projects => res.json(projects))
+  queries.getAllProjects().then(projects => res.render('projects', { projects }))
 })
 
 router.get('/:id', (req, res) => {
