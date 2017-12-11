8a2e854abc9b71e36d6dd223e5d2f29b2abefcec
matt-winzer <matt.winzer@gmail.com>
2017-12-11 16:41:57 -0700

    updated project migration

--- migrations/20171208110644_02_project.js
+++ migrations/20171208110644_02_project.js
@@ -1,9 +1,11 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('project', (table) =>{
     table.increments()
-    table.text('projectName').notNullable()
-    table.text('projectUrl').notNullable()
-    table.text('projectGit')
+    table.text('name').notNullable()
+    table.text('description')
+    table.text('imageUrl')
+    table.text('deployedUrl').notNullable()
+    table.text('gitUrl')
     table.integer('memberId').references('member.id').unsigned().onDelete('cascade')
   })
 }
