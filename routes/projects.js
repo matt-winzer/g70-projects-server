8bb20dc155a607e2551dd205150ac505750824b8
matt-winzer <matt.winzer@gmail.com>
2017-12-13 16:25:39 -0700

    student names dispay in cohort sorted groups on submitpage

--- routes/projects.js
+++ routes/projects.js
@@ -4,11 +4,15 @@ const knex = require('../db/connection')
 const queries = require('../db/queries')
 
 router.get('/', (req, res) => {
-  queries.getAllProjects().then(projects => res.render('projects', { title: 'gProjects', projects }))
+  queries.getAllProjects().then(projects => res.render('projects', { title: 'All Projects', projects }))
 })
 
 router.get('/submit', (req, res) => {
-  queries.getAllStudents().then(students => res.render('submit', { students }))
+  queries.getAllStudents().then(students => {
+    const g70 = students.filter(student => student.cohort.toLowerCase() === 'g70')
+    const g75 = students.filter(student => student.cohort.toLowerCase() === 'g75')
+    res.render('submit', { g70, g75 })
+  })
 })
 
 router.get('/:id', (req, res) => {
